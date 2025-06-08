f4307e16237765e9412d85e82a1a770c2bb2adc1
도미닉 <kiljh@me.com>
2025-06-08 15:02:26 +0900

    CreatOrderPresenter 테스트 작성

--- CleanStoreTests/CreateOrder/CreateOrderPresenterTests.swift
+++ CleanStoreTests/CreateOrder/CreateOrderPresenterTests.swift
@@ -8,28 +8,18 @@
 import XCTest
 
 final class CreateOrderPresenterTests: XCTestCase {
-
+    var createOrderPresenter: CreateOrderPresenter!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        setupCreateOrderPresenter()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func setupCreateOrderPresenter() {
+        createOrderPresenter = CreateOrderPresenter()
     }
 
     class CreateOrderDisplayLogicSpy: CreateOrderDisplayLogic {
@@ -40,8 +30,41 @@ final class CreateOrderPresenterTests: XCTestCase {
             displayExpirationDateCalled = true
             self.formatExpirationDateViewModel = viewModel
         }
+    }
+    
+    func testPresentExpirationDateShouldConvertDateToString()
+    {
+        // Given
+        let createOrderDisplayLogicSpy = CreateOrderDisplayLogicSpy()
+        createOrderPresenter.viewController = createOrderDisplayLogicSpy
         
+        var dateComponents = DateComponents()
+        dateComponents.year = 2007
+        dateComponents.month = 6
+        dateComponents.day = 29
+        let calendar = Calendar.current
+        let date = calendar.date(from: dateComponents) ?? Date()
+        let response = CreateOrder.FormatExpirationDate.Response(date: date)
         
+        // When
+        createOrderPresenter.presentExpirationDate(response: response)
         
+        // Then
+        let returnedDate = createOrderDisplayLogicSpy.formatExpirationDateViewModel.date
+        let expectedDate = "6/29/07"
+        XCTAssertEqual(returnedDate, expectedDate, "Presenting an expiration date should convert date to string")
+    }
+    
+    func testPresentExpirationDateShouldAskViewControllerToDisplayDateString() {
+        // Given
+        let createOrderDisplayLogicSpy = CreateOrderDisplayLogicSpy()
+        createOrderPresenter.viewController = createOrderDisplayLogicSpy
+        let response = CreateOrder.FormatExpirationDate.Response(date: Date())
+        
+        // When
+        createOrderPresenter.presentExpirationDate(response: response)
+        
+        // Then
+        XCTAssert(createOrderDisplayLogicSpy.displayExpirationDateCalled, "Presenting an expiration date should ask view controller to display date string")
     }
 }
